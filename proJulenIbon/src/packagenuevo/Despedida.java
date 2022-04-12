01b96353e4fbd0841101010c5a6c25c0b8522204
ROOT <ROOT@ENT18WEB>
2022-04-12 10:16:20 +0200

    Primera version rama

--- proJulenIbon/src/packagenuevo/Despedida.java
+++ proJulenIbon/src/packagenuevo/Despedida.java
@@ -4,7 +4,7 @@ public class Despedida {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Primer syso de la nueva rama");
+		System.out.println("Primer Syso de la nueva rama");
 
 	}
 
