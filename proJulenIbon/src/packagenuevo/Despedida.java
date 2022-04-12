695e66bc4e28b739fc9a127fbe6b369302690a46
ROOT <ROOT@ENT18WEB>
2022-04-12 10:12:48 +0200

    Primera version de la nueva rama

---
+++ proJulenIbon/src/packagenuevo/Despedida.java
@@ -0,0 +1,11 @@
+package packagenuevo;
+
+public class Despedida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer syso de la nueva rama");
+
+	}
+
+}
